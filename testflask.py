950f88d5cf3eb44a02ca0bdc2793fed67123049d
thang.cao <thang.cao@9thwonder.com>
2020-06-19 13:41:36 +0700

    update search

--- testflask.py
+++ testflask.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
   return render_template('index.html', content=['thang', 'thang cao'])
+
+@app.route("/search")
+def search():
+  keyword = request.args.get('keyword')
+  return render_template('search.html', keyword = keyword)
 
 if __name__ == "__main__":
   app.run()
--- testowl.py
+++ testowl.py
@@ -1,9 +1,28 @@
+# Load Ontology
 from owlready2 import *
 onto = get_ontology("./qlsv.owl").load()
-text = ""
-searchResult = onto.search(Hoc = onto.search_one(Ten = "Sematic Web"))
-print(searchResult)
-for x in searchResult:
-  text += " " + x.name
 
-print(text)
+# Region web
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template('index.html', content=['thang', 'thang cao'])
+
+@app.route("/search")
+def search():
+  keyword = request.args.get('keyword')
+  if keyword is None:
+    keyword = ''
+  searchResult = []
+  monHoc = onto.search_one(Ten = "*"+ keyword +"*")
+  if monHoc:
+    for x in onto.search(Hoc = monHoc):
+      searchResult.append(x.name)
+
+  return render_template('search.html', keyword = keyword, searchResult = searchResult)
+
+if __name__ == "__main__":
+  app.run()
